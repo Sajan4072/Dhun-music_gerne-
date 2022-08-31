36b5b2f14b348efc74ebc526aefb79d904d8b42d
Sajan4072 <rai4072@gmail.com>
2022-08-31 18:15:47 +0545

    final build

--- Music-Genre-Classification-Django-master/MusicClassification/urls.py
+++ Music-Genre-Classification-Django-master/MusicClassification/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('predictor.urls')),
+    path('',include('predictor.urls')),
     path('admin/', admin.site.urls)
 ]
 
--- Music-Genre-Classification-Django-master/predictor/urls.py
+++ Music-Genre-Classification-Django-master/predictor/urls.py
@@ -15,10 +15,21 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'predictor'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('',views.log,name='log'),
+    path('login',views.login_view,name='login'),
+    path('logout',views.logout_view,name='logout'),
+    path('register/',views.registerPage,name='register'),
+    path('blogs',views.blogs,name='blogs'),
+    path('about',views.about,name='about'),
+    path('add',views.add,name='add'),
+    path('post',views.post,name='post'),
+    path('detail/<int:id>',views.detail,name='detail'),
+    path('index', views.IndexView.as_view(), name='index'),
     path('result/',views.model_form_upload,name = 'result'),
 ]
